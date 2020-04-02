6dd18e390e12695c7597f3f944fdde4bdc0b863c
dmontes0103 <dmontes0103@gmail.com>
2020-04-02 00:38:42 -0600

    Initial

--- deploy.py
+++ deploy.py
@@ -10,8 +10,12 @@ app = Flask(__name__)
 
 @app.route('/api/reports/districts')
 def index():
-    csvResponse = requests.get("http://geovision.uned.ac.cr/oges/archivos_covid/03_31/03_31_CSV.csv")
+    csvResponse = requests.get("http://geovision.uned.ac.cr/oges/archivos_covid/03_30/03_30_CSV.csv")
     #print(csvResponse.text)    
     reader = csv.DictReader(io.StringIO(csvResponse.text))
     json_data = json.dumps(list(reader))
     return json_data
+
+@app.route('/')
+def home():
+    return '<h2>Welcome to Districts API</h1>'
