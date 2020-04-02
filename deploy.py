658b731324bbf00bc59137b03fb4284496274a77
dmontes0103 <dmontes0103@gmail.com>
2020-04-02 00:30:13 -0600

    Initial

---
+++ deploy.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import csv
+import io
+from flask import Flask
+
+app = Flask(__name__)
+
+#print('JSON:',json_data)
+
+@app.route('/api/reports/districts')
+def index():
+    csvResponse = requests.get("http://geovision.uned.ac.cr/oges/archivos_covid/03_30/03_30_CSV.csv")
+    print(csvResponse.text)    
+    reader = csv.DictReader(io.StringIO(csvResponse.text))
+    json_data = json.dumps(list(reader))
+    return '<h1> Deployted </h1>'
