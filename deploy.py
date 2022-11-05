9965b670b3df2cefe015bf15d31b5f10afd6bff0
dmontes0103 <dmontes0103@gmail.com>
2022-11-04 18:40:17 -0600

    adding csv mocking file,chaning flask response return

--- deploy.py
+++ deploy.py
@@ -1,11 +1,9 @@
 import requests
-import json
-import csv
-import io
+import json,csv,io
+from test import csv_to_json
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from datetime import date,datetime, timedelta
-import ftfy
 
 app = Flask(__name__)
 
@@ -18,19 +16,17 @@ def index():
     x = datetime.now() - timedelta(days=1)
     mes = x.strftime("%m")
     dia = x.strftime("%d")
-    url = f"http://geovision.uned.ac.cr/oges/archivos_covid/{mes}_{dia}/{mes}_{dia}_CSV.csv"
+    #url = f"http://geovision.uned.ac.cr/oges/archivos_covid/{mes}_{dia}/{mes}_{dia}_CSV.csv"
     try:
-        csvResponse = requests.get(url)
+        data = csv_to_json(r'data\dummydata.csv')
     except Exception as err:
-        return("<h1>Error ocurred while retrieving data</h1>", err)    
-    reader = csv.DictReader(io.StringIO(csvResponse.text))    
-    json_data = json.dumps(list(reader))    
-    return json_data
+        return "Error converting file"
+    return app.response_class(response=json.dumps(data), status=200, mimetype='application/json')
     
 
 @app.route('/')
 def home():
-    return '<h2>Welcome to Districts API</h1>'
+    return '<h2>Welcome to Cantones API</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ test.py
@@ -0,0 +1,25 @@
+import csv 
+import json 
+
+def csv_to_json(csvFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+    return jsonArray
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray)
+        jsonf.write(jsonString)
+          
+csvFilePath = r'data\dummydata.csv'
+
+
+#print(csv_to_json(csvFilePath))
